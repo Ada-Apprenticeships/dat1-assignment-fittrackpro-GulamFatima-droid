afbce204a0dc4cfe359b6f556e1b295cd4dc41ff
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2025-02-05 17:07:20 +0000

    commit 5

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -8,16 +8,23 @@ PRAGMA foreign_keys = ON;
 
 -- 1. List all classes with their instructors
 
-WITH inst i AS 
-(
-    SELECT staff_id, first_name
-    FROM staff
-)
-
-SELECT 
+SELECT c.class_id, c.name, s.first_name
+FROM classes c 
+LEFT JOIN class_schedule cs, staff s
+ON cs.class_id = c.class_id AND cs.staff_id = s.staff_id;
 
 -- 2. Find available classes for a specific date
--- TODO: Write a query to find available classes for a specific date
+
+SELECT cs.class_id, cs.start_time, cs.end_time, c.name, c.capacity, cs.schedule_id,
+(
+SELECT COUNT(*) 
+FROM class_attendance ca
+WHERE cs.schedule_id = ca.schedule_id
+AND ca.attendance_status = 'Registered'
+)
+FROM class_schedule cs 
+LEFT JOIN classes c ON c.class_id = cs.class_id
+WHERE cs.start_time LIKE '2025-02-01%';
 
 -- 3. Register a member for a class
 -- TODO: Write a query to register a member for a class
