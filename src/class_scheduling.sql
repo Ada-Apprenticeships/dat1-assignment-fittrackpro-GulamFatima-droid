42d3841c63d3643f9c2e1b1f4dcdd92aec186c3b
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2025-02-06 15:07:55 +0000

    commit 6

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -15,25 +15,49 @@ ON cs.class_id = c.class_id AND cs.staff_id = s.staff_id;
 
 -- 2. Find available classes for a specific date
 
-SELECT cs.class_id, cs.start_time, cs.end_time, c.name, c.capacity, cs.schedule_id,
-(
-SELECT COUNT(*) 
+SELECT cs.class_id, c.name, cs.start_time, cs.end_time, (c.capacity - 
+(SELECT COUNT(*) 
 FROM class_attendance ca
 WHERE cs.schedule_id = ca.schedule_id
-AND ca.attendance_status = 'Registered'
-)
+AND ca.attendance_status = 'Registered')) AS available_spots
 FROM class_schedule cs 
 LEFT JOIN classes c ON c.class_id = cs.class_id
 WHERE cs.start_time LIKE '2025-02-01%';
 
 -- 3. Register a member for a class
--- TODO: Write a query to register a member for a class
+
+INSERT INTO class_attendance (schedule_id, member_id, attendance_status)
+VALUES (
+(SELECT schedule_id 
+FROM class_schedule 
+WHERE class_id = 3 
+AND start_time LIKE '2025-02-01%'),11,'Registered');
 
 -- 4. Cancel a class registration
--- TODO: Write a query to cancel a class registration
+
+DELETE FROM class_attendance
+WHERE schedule_id =  7 
+AND member_id = 2;
 
 -- 5. List top 5 most popular classes
--- TODO: Write a query to list top 5 most popular classes
+
+SELECT cs.class_id, c.name,
+(SELECT COUNT(*) 
+FROM class_attendance ca
+WHERE cs.schedule_id = ca.schedule_id
+AND ca.attendance_status = 'Registered') AS registration_count
+FROM class_schedule cs 
+LEFT JOIN classes c ON c.class_id = cs.class_id
+ORDER BY registration_count DESC
+LIMIT 3;
 
 -- 6. Calculate average number of classes per member
--- TODO: Write a query to calculate average number of classes per member
+
+WITH attendances AS 
+(
+SELECT m.member_id, (SELECT COUNT(*) FROM class_attendance ca WHERE m.member_id = ca.member_id) AS count
+FROM members m
+)
+
+SELECT AVG(count) AS average_no_classes
+FROM attendances;
