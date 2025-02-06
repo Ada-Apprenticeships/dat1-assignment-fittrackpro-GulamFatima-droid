2f4f5378b04115da3864c72ade1d149868d61865
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2025-02-06 16:10:29 +0000

    commit 7

--- README.md
+++ README.md
@@ -242,12 +242,12 @@ Use this as a checklist to keep track of your progress.
 6. [x] 4.6 Calculate average number of classes per member
 
 #### 5. Membership Management (membership_management.sql)
-1. [ ] 5.1 List all active memberships
-2. [ ] 5.2 Calculate the average duration of gym visits for each membership type
-3. [ ] 5.3 Identify members with expiring memberships this year
+1. [x] 5.1 List all active memberships
+2. [x] 5.2 Calculate the average duration of gym visits for each membership type
+3. [x] 5.3 Identify members with expiring memberships this year
 
 #### 6. Attendance Tracking (attendance_tracking.sql)
-1. [ ] 6.1 Record a member's gym visit
+1. [x] 6.1 Record a member's gym visit
 2. [ ] 6.2 Retrieve a member's attendance history
 3. [ ] 6.3 Find the busiest day of the week based on gym visits
 4. [ ] 6.4 Calculate the average daily attendance for each location
--- src/membership_management.sql
+++ src/membership_management.sql
@@ -4,15 +4,52 @@
 
 PRAGMA foreign_keys = ON;
 
--- Enable foreign key support
-
 -- Membership Management Queries
 
 -- 1. List all active memberships
--- TODO: Write a query to list all active memberships
+
+SELECT m.member_id, m.first_name, m.last_name, me.type, m.join_date
+FROM members m
+LEFT JOIN memberships me ON m.member_id = me.member_id
+WHERE me.status = 'Active';
 
 -- 2. Calculate the average duration of gym visits for each membership type
--- TODO: Write a query to calculate the average duration of gym visits for each membership type
+
+WITH 
+startTime AS (  
+    SELECT member_id, 
+        (strftime('%H', check_in_time) * 60 +  
+         strftime('%M', check_in_time) +  
+         strftime('%S', check_in_time) / 60) 
+         AS startMin  
+    FROM attendance  
+), 
+endTime AS (  
+    SELECT member_id, 
+        (strftime('%H', check_out_time) * 60 +  
+         strftime('%M', check_out_time) +  
+         strftime('%S', check_out_time) / 60) 
+         AS endMin  
+    FROM attendance  
+),
+timeDiff AS (
+    SELECT s.member_id, (e.endMin - s.startMin) AS timeSpent
+    FROM startTime s, endTime e
+    WHERE e.member_id = s.member_id
+),
+result AS (
+    SELECT t.timeSpent, t.member_id, (SELECT type FROM memberships me WHERE me.member_id = t.member_id) AS type
+    FROM timeDiff t
+)
+
+SELECT r.type AS membership_type, AVG(r.timeSpent) AS avg_visit_duration_minutes
+FROM result r 
+GROUP BY r.type;
+
 
 -- 3. Identify members with expiring memberships this year
--- TODO: Write a query to identify members with expiring memberships this year
+
+SELECT m.member_id, m.first_name, m.last_name, m.email, me.end_date
+FROM members m 
+LEFT JOIN memberships me ON me.member_id = m.member_id
+WHERE strftime('%Y', me.end_date) = strftime('%Y', 'now');
